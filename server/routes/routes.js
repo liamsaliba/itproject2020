6a487e297b91529dafda9e8cfc2854b2020180c9
Liam Saliba <liamsaliba@hotmail.com>
2020-08-31 11:52:46 +1000

    Re-add /api call for api testing

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,4 +6,9 @@ const userRouter = require("./user.route");
 
 router.use("/user", userRouter);
 
+router.get("/", (req, res) => {
+  res.set("Content-Type", "application/json");
+  res.send('{"message": "Hello from express!"}');
+});
+
 module.exports = router;
