d795da8c1fd1bf6db9ee885af7858e7e2938f1fc
Liam Saliba <liamsaliba@hotmail.com>
2020-08-31 11:04:16 +1000

    Fix logo path

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import logo from "../svg/logo.svg";
+import logo from "../../svg/logo.svg";
 import "./App.css";
 
 function App() {
