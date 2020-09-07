0e0a4f41ede179bf91001f9cfc04386ee852d656
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-09-07 22:05:01 +1000

    Added fields - firstname, lastname, email, username, password, confirm password - to the sign-up page!

--- client/src/components/pages/SignUp.jsx
+++ client/src/components/pages/SignUp.jsx
@@ -15,10 +15,24 @@ export default () => {
         textAlign:'center', 
         justifyContent: 'center'}}
     >
+      <Label htmlFor="First Name">First Name</Label>
+      <Input name="firstName" mb={1} />
+
+      <Label htmlFor="Last Name">Last Name</Label>
+      <Input name="lastName" mb={1} />
+
+      <Label htmlFor="email">email</Label>
+      <Input name="email" mb={1} />
+
       <Label htmlFor="username">Username</Label>
-      <Input name="username" mb={3} />
+      <Input name="username" mb={1} />
+
       <Label htmlFor="password">Password</Label>
-      <Input type="password" name="password" mb={3} />
+      <Input type="password" name="password" mb={1} />
+
+      <Label htmlFor="confirm password">Confirm Password</Label>
+      <Input type="confirmPassword" name="confirmPassword" mb={3} />
+
       <Box>
         <Label mb={3}>
           <Checkbox />
