aafbb659a6348b11e6153db05a60833da20a1b0b
Liam Saliba <liamsaliba@hotmail.com>
2020-09-07 21:05:25 +1000

    remove console log

--- client/src/components/editor/Navbar.jsx
+++ client/src/components/editor/Navbar.jsx
@@ -3,7 +3,6 @@ import { jsx, Box, Image, Flex } from "theme-ui";
 import { MenuItem } from "../NavItems";
 
 export default ({ userId: id }) => {
-  console.log(id);
   return (
     <Box p={10} bg="secondary">
       <Flex as="nav">
