a7f66dafcde844009a067c1d81af1e980c5b2362
Josh Nguyen <tuandungnguyen99@gmail.com>
2020-09-24 03:05:29 +1000

    Add new notifications

--- server/emailbot/email.js
+++ server/emailbot/email.js
@@ -50,7 +50,7 @@ const sendGreeting = (email, user) => {
 //    email      : a string of list of strings
 //    user       : the user object
 //    changeItems: list of strings for item names
-const sendChangeNotification = (email, user, changeItem) => {
+const sendAccountChangeNotification = (email, user, changeItem) => {
   if (!email) {
     return;
   }
@@ -64,7 +64,7 @@ const sendChangeNotification = (email, user, changeItem) => {
     changeItem = changeItem[0];
   }
   // Read the pre-compiled message
-  fs.readFile("./emailbot/samples/change-noti.txt", "utf-8", (err, data) => {
+  fs.readFile("./emailbot/samples/account-change.txt", "utf-8", (err, data) => {
     if (err) return;
 
     let name = user.username;
@@ -79,7 +79,7 @@ const sendChangeNotification = (email, user, changeItem) => {
     const mailOptions = {
       from: process.env.EMAIL_ADDRESS,
       to: email,
-      subject: "Camel Pages: Change of details",
+      subject: "Camel Pages: Change of Account Details",
       text: data,
     };
     transporter.sendMail(mailOptions, (err, _info) => {
@@ -90,4 +90,91 @@ const sendChangeNotification = (email, user, changeItem) => {
   });
 };
 
-module.exports = { sendGreeting, sendChangeNotification };
+// Send a notification email to a user about the addition of a portfolio
+// args:
+//    email      : a string of list of strings
+//    user       : the user object
+const sendPortfolioAddNotification = (email, user) => {
+  if (!email) {
+    return;
+  }
+  // Read the pre-compiled message
+  fs.readFile("./emailbot/samples/portfolio-add.txt", "utf-8", (err, data) => {
+    if (err) return;
+
+    let name = user.username;
+    if (user.local && user.local.firstName && user.local.lastName) {
+      name = user.local.firstName + " " + user.local.lastName;
+    }
+
+    data = data.replace("${username}", name);
+
+    const mailOptions = {
+      from: process.env.EMAIL_ADDRESS,
+      to: email,
+      subject: "Camel Pages: New Portfolio Created!",
+      text: data,
+    };
+    transporter.sendMail(mailOptions, (err, _info) => {
+      if (err) {
+        return;
+      }
+    });
+  });
+};
+
+// Send a notification email to a user about the change of some item in their portfolio
+// args:
+//    email      : a string of list of strings
+//    user       : the user object
+//    changeItems: list of strings for item names
+const sendPortfolioChangeNotification = (email, user, changeItem) => {
+  if (!email) {
+    return;
+  }
+  if (!changeItem || (changeItem instanceof Array && changeItem.length == 0)) {
+    return;
+  }
+  if (changeItem.length > 1) {
+    changeItem = changeItem.filter(i => i != null);
+    changeItem = "\n\n    - " + changeItem.join("\n    - ");
+  } else {
+    changeItem = changeItem[0];
+  }
+  // Read the pre-compiled message
+  fs.readFile(
+    "./emailbot/samples/portfolio-change.txt",
+    "utf-8",
+    (err, data) => {
+      if (err) return;
+
+      let name = user.username;
+      if (user.local && user.local.firstName && user.local.lastName) {
+        name = user.local.firstName + " " + user.local.lastName;
+      }
+
+      data = data
+        .replace("${username}", name)
+        .replace("${changeItem}", changeItem);
+
+      const mailOptions = {
+        from: process.env.EMAIL_ADDRESS,
+        to: email,
+        subject: "Camel Pages: Change of Portfolio Details",
+        text: data,
+      };
+      transporter.sendMail(mailOptions, (err, _info) => {
+        if (err) {
+          return;
+        }
+      });
+    }
+  );
+};
+
+module.exports = {
+  sendGreeting,
+  sendAccountChangeNotification,
+  sendPortfolioAddNotification,
+  sendPortfolioChangeNotification,
+};
