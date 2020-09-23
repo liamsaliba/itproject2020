b081b14f6d4404adf5652915c2a1ce561c2dc4ec
Chan Jie Ho <hochanjie@hotmail.com>
2020-09-23 16:25:55 +1000

    Updated comments
    Took lines of code out of comments for the loading story

--- client/src/components/Sidebar/PageList.jsx
+++ client/src/components/Sidebar/PageList.jsx
@@ -9,26 +9,26 @@ export function PageList({ loading, pages }, onEditPage, onDeletePage) {
     onDeletePage,
   };
 
-  // const LoadingRow = (
-  //   <div className="loading-item">
-  //     <span className="glow-checkbox" />
-  //     <span className="glow-text">
-  //       <span>Loading</span> <span>cool</span> <span>state</span>
-  //     </span>
-  //   </div>
-  // );
-  // if (loading) {
-  //   return (
-  //     <div className="list-items">
-  //       {LoadingRow}
-  //       {LoadingRow}
-  //       {LoadingRow}
-  //       {LoadingRow}
-  //       {LoadingRow}
-  //       {LoadingRow}
-  //     </div>
-  //   );
-  // }
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+  if (loading) {
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
 
   return (
     <div className="list-items">
