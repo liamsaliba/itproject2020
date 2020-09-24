49cfd8419253df167ba4f1db2b91d159d48e90b1
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-09-24 11:21:54 +1000

    Added Story for NavItem
    Added storybook-react-router to package.json
    Added Story for NavItem

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,17 @@
+/** @jsx jsx */
+import { addDecorator } from '@storybook/react';
 
+import { jsx, ThemeProvider } from 'theme-ui';
+import themes from "../src/themes";
+
+const theme = themes.custom;
+
+addDecorator(storyFn => (
+  <ThemeProvider theme={theme}>
+    {storyFn()}
+  </ThemeProvider>
+));
+/* 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+} */
