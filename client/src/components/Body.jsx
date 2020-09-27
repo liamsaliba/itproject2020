27f445150cf5963b2eb14b7125c299baf035a87c
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-09-27 11:20:03 +1000

    Updated Body
    More Reusable and configured Arguments

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -2,7 +2,7 @@
 import { jsx, Styled, Button, Container } from "theme-ui";
 import PropTypes from "prop-types";
 
-export default function Body({ body: { hAlign, vAlign, hasHeading, hasBody, hasAction } }) {
+export default function Body({ body: { hAlign, vAlign, heading, body, actionString, onAction } }) {
   const styling = {
     padding: "5px",
     textAlign: hAlign,
@@ -10,32 +10,22 @@ export default function Body({ body: { hAlign, vAlign, hasHeading, hasBody, hasA
   };
 
   const Heading = () => {
-    if (hasHeading) {
-      return (
-        <Styled.h2>Body Title</Styled.h2>
-      );
-    } return;
+    return (
+      <Styled.h2>{heading}</Styled.h2>
+    );
   }
 
   const Text = () => {
-    if (hasBody) {
-      return (
-        <Styled.p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias
-          officiis quam et iste ratione earum illo aliquid neque, quis quas
-          accusamus voluptatum provident dolorum aspernatur nostrum quo similique
-          odit!
-        </Styled.p>
-      );
-      
-    } return;
+    return (
+      <Styled.p>{body}</Styled.p>
+    );
   }
 
   const out = (
     <Container sx={styling}>
       <Heading />
       <Text />
-      {hasAction && <Button>Optional Action</Button>}
+      {actionString && <Button onClick={() => onAction}>{actionString}</Button>}
     </Container>
   );
   return out;
@@ -46,8 +36,9 @@ Body.propTypes = {
   body: PropTypes.shape({
     hAlign: PropTypes.string,
     vAlign: PropTypes.string,
-    hasHeading: PropTypes.bool,
-    hasBody: PropTypes.bool,
-    hasAction: PropTypes.bool,
+    heading: PropTypes.bool,
+    body: PropTypes.bool,
+    actionString: PropTypes.bool,
+    onAction: PropTypes.func,
   }),
 };
--- client/src/components/stories/Body.stories.js
+++ client/src/components/stories/Body.stories.js
@@ -9,57 +9,59 @@ export default {
 
 const Template = args => <Body {...args} />;
 
+const Default = {
+  body: {
+    hAlign: "center",  
+    vAlign: "middle", 
+    heading: "Display Title", 
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias\
+      officiis quam et iste ratione earum illo aliquid neque, quis quas\
+      accusamus voluptatum provident dolorum aspernatur nostrum quo similique\
+      odit!", 
+    actionString: "Optional Action", 
+    onAction: null,
+  }
+}
+
 export const Centered = Template.bind({});
 Centered.args = {
   body: {
-    hAlign: "center",
-    vAlign: "middle",
-    hasHeading: true, 
-    hasBody: true,
-    hasAction: true,
+    ...Default.body
   },
 };
 
 export const TopRight = Template.bind({});
 TopRight.args = {
   body: {
+    ...Default.body,
     hAlign: "right",
     vAlign: "top",
-    hasHeading: true, 
-    hasBody: true,
-    hasAction: true,
   },
 };
 
 export const TopLeft = Template.bind({});
 TopLeft.args = {
   body: {
+    ...Default.body,
     hAlign: "left",
     vAlign: "top",
-    hasHeading: true, 
-    hasBody: true,
-    hasAction: true,
   },
 };
 
 export const BottomRight = Template.bind({});
 BottomRight.args = {
   body: {
+    ...Default.body,
     hAlign: "right",
     vAlign: "bottom",
-    hasHeading: true, 
-    hasBody: true,
-    hasAction: true,
   },
 };
 
 export const BottomLeft = Template.bind({});
 BottomLeft.args = {
   body: {
+    ...Default.body,
     hAlign: "left",
     vAlign: "bottom",
-    hasHeading: true, 
-    hasBody: true,
-    hasAction: true,
   },
 };
