ea4448884015df2e8937b21a7d20abfb0d9ad09f
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-09-27 13:23:56 +1000

    Added ability to style Body

--- client/src/components/stories/Body.stories.js
+++ client/src/components/stories/Body.stories.js
@@ -14,10 +14,10 @@ const Default = {
     hAlign: "center",  
     vAlign: "middle", 
     heading: "Display Title", 
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias\
-      officiis quam et iste ratione earum illo aliquid neque, quis quas\
-      accusamus voluptatum provident dolorum aspernatur nostrum quo similique\
-      odit!", 
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora alias" +
+          " officiis quam et iste ratione earum illo aliquid neque, quis quas" +
+          " accusamus voluptatum provident dolorum aspernatur nostrum quo similique" +
+          " odit!",
     actionString: "Optional Action", 
     onAction: null,
   }
