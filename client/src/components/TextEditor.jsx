14d80988f5d2475306651963a6f0155d7da07a61
Liam Saliba <liamsaliba@hotmail.com>
2020-09-30 09:44:05 +1000

    It does render, but we've still got <p>

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import { Button, Icon, Modal } from "semantic-ui-react";
 import { Editor } from "@tinymce/tinymce-react";
 
+const defaultText =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate \
+nisi quibusdam saepe. Commodi temporibus, atque rem unde vel, \
+voluptatem tempore quisquam fugit exercitationem voluptates sint. \
+Porro temporibus quisquam eveniet molestiae.";
+
 export default function TextEditor() {
   const [open, setOpen] = useState(false);
 
+  const [text, setText] = useState(defaultText);
+
   const handleChange = (content, editor) => {
-    console.log("Content was updated:", content);
+    console.log("Content was updated:", setText(content));
   };
 
   return (
@@ -17,17 +25,10 @@ export default function TextEditor() {
       onOpen={() => setOpen(true)}
       open={open}
       size="FullScreen"
-      trigger={
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-          nisi quibusdam saepe. Commodi temporibus, atque rem unde vel,
-          voluptatem tempore quisquam fugit exercitationem voluptates sint.
-          Porro temporibus quisquam eveniet molestiae.
-        </p>
-      }
+      trigger={<p>{text}</p>}
     >
       <Editor
-        initialValue="<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nisi quibusdam saepe. Commodi temporibus, atque rem unde vel, voluptatem tempore quisquam fugit exercitationem voluptates sint. Porro temporibus quisquam eveniet molestiae.</p>"
+        initialValue={`${text}`}
         apiKey="n5ht6rb15nn13oiatqpzcgq6s67wressz3ux6hme9kp6dbht"
         init={{
           skin: "oxide-dark",
@@ -35,6 +36,7 @@ export default function TextEditor() {
           statusbar: false,
           height: 600,
           menubar: false,
+          outputFormat: "text",
           plugins: [
             "advlist autolink lists link image charmap preview code fullscreen media table paste codesample",
           ],
