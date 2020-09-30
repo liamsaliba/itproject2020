63459f24f9711f811c25bf332c01304a60856843
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-09-30 11:18:31 +1000

    Integrated fields as modal into Artefacts OnClick

--- client/src/components/Artefacts.jsx
+++ client/src/components/Artefacts.jsx
@@ -3,10 +3,14 @@ import { jsx, Flex, Box, Image } from "theme-ui";
 import PropTypes from "prop-types";
 import documentPreview from "../svg/DocumentPreview.png";
 import { Icon } from 'semantic-ui-react';
+import { useState } from 'react';
 
 import Body from './Body';
 
-export default function Artefact({ artefact: { media, hPos, vPos, style }, body, onAddDocument}) {
+import ArtefactField from './ArtefactField';
+
+export default function Artefact({ isEditing, artefact: { media, hPos, vPos, style }, body, onAddDocument}) {
+  const [open, setOpen] = useState(false);
 
   const styling = {
     ...style,
@@ -38,17 +42,35 @@ export default function Artefact({ artefact: { media, hPos, vPos, style }, body,
     </Box>
   );
 
+  const handleClick = () => {
+    if (isEditing) {
+      setOpen(!open)
+    }
+  }
+
+  const artefactFieldArgs = {
+    state:{
+      open: open, 
+      setOpen: setOpen
+    }, 
+    artefactField:{
+      isNew: false
+    }, 
+  }
+
   const Out = () => {
     if (hPos==="left") {
       return (
-        <Flex>
+        <Flex onClick={handleClick}>
+          <ArtefactField {...artefactFieldArgs}/>
           <MediaCollection />
           <Body body={body}/>
         </Flex> 
       );
     } else if (hPos==="right") {
       return (
-        <Flex>
+        <Flex onClick={handleClick}>
+          <ArtefactField {...artefactFieldArgs}/>
           <Body body={body}/>
           <MediaCollection />
         </Flex> 
@@ -63,6 +85,7 @@ export default function Artefact({ artefact: { media, hPos, vPos, style }, body,
 
 Artefact.propTypes = {
   /** Composition of the page */
+  isEditing: PropTypes.bool,
   artefact: PropTypes.shape({
     media: PropTypes.string,
     hPos: PropTypes.string,
