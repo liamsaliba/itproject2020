f27c05eab05a737ea1287a74eaef67dfe4c3e881
Liam Saliba <liamsaliba@hotmail.com>
2020-09-30 22:28:35 +1000

    fix not found

--- client/src/pages/Main/NotFound.jsx
+++ client/src/pages/Main/NotFound.jsx
@@ -5,20 +5,9 @@ import { Styled, Box, Container } from "theme-ui";
 
 export default () => {
   return (
-    <React.Fragment>
-      <Container
-        variant="layout.centerflex"
-        sx={{
-          p: 10,
-          backgroundImage:
-            "url('https://cdn.pixabay.com/photo/2019/09/19/00/40/fantasy-4487980_1280.jpg')",
-        }}
-      >
-        <Box p={10} sx={{ position: "absolute", top: "10%", left: "10%" }}>
-          <Styled.h1>404</Styled.h1>
-          <Styled.p>Have you lost your way?</Styled.p>
-        </Box>
-      </Container>
-    </React.Fragment>
+    <Box>
+      <Styled.h1>404</Styled.h1>
+      <Styled.p>Have you lost your way?</Styled.p>
+    </Box>
   );
 };
