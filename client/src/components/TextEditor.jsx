f504a30218b5dadaeddc25264b34421b6af57d86
Chan Jie Ho <hochanjie@hotmail.com>
2020-09-30 10:26:14 +1000

    Added HTML Parser and got rid of buttons
    Now returns text as we see within the editor

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Icon, Modal } from "semantic-ui-react";
 import { Editor } from "@tinymce/tinymce-react";
+import ReactHTMLParser from "react-html-parser";
 
 const defaultText =
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate \
@@ -20,12 +21,11 @@ export default function TextEditor() {
   return (
     <Modal
       basic
-      closeOnDimmerClick={false}
       onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
       open={open}
       size="FullScreen"
-      trigger={<p>{text}</p>}
+      trigger={<p>{ReactHTMLParser(text)}</p>}
     >
       <Editor
         initialValue={`${text}`}
@@ -41,19 +41,11 @@ export default function TextEditor() {
             "advlist autolink lists link image charmap preview code fullscreen media table paste codesample",
           ],
           toolbar:
-            "undo redo | formatselect | bold italic underline | link codesample code | alignleft aligncenter" +
-            "alignright alignjustify | bullist numlist ",
+            "undo redo | formatselect | bold italic underline | link codesample code | " +
+            "alignleft aligncenter alignright alignjustify | bullist numlist ",
         }}
         onEditorChange={handleChange}
       />
-      <Modal.Actions>
-        <Button basic color="red" onClick={() => setOpen(false)}>
-          <Icon name="remove" /> Cancel
-        </Button>
-        <Button color="green" onClick={() => setOpen(false)}>
-          <Icon name="checkmark" /> Save
-        </Button>
-      </Modal.Actions>
     </Modal>
   );
 }
