a1a99fbcab861351c9b885b1ffbae6764c19e125
Liam Saliba <liamsaliba@hotmail.com>
2020-10-17 15:07:54 +1100

    move Cards to Card

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Box, Image, Styled, Card as ThemedCard } from "theme-ui";
+import { jsx, Image, Styled, Card as ThemedCard } from "theme-ui";
 import React from "react";
 import PropTypes from "prop-types";
 import { Section } from "./Section";
@@ -76,33 +76,3 @@ export const Cards = props => (
     ))}
   />
 );
-
-export const Cards = ({ key, pageId, name, cards }) => {
-  // TODO: Where to use pageId?s
-  const styling = {
-    margin: "0 auto",
-    mb: "2em",
-    display: "flex",
-    flexFlow: "row wrap", // the wrap & flexDir makes all the difference here.
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    transition: "all 0.3s",
-  };
-
-  let allCards = cards.map((card, index) => <Card key={key} {...card} />);
-
-  return (
-    <Box sx={{ textAlign: "center" }} id={name}>
-      <Styled.h2 id={name}> {name} </Styled.h2>
-      <Box sx={styling} mr={5} ml={5}>
-        {allCards}
-      </Box>
-    </Box>
-  );
-};
-
-Cards.propTypes = {
-  name: PropTypes.string,
-  cards: PropTypes.array,
-};
