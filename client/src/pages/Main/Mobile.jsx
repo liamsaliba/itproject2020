60cec393e6a56e7661122cf8bfc4f46b60fd3dc1
Chan Jie Ho <hochanjie@hotmail.com>
2020-10-20 07:29:49 +1100

    Added portfolio and logout options in landing page mobile sidebar

--- client/src/pages/Main/Mobile.jsx
+++ client/src/pages/Main/Mobile.jsx
@@ -3,29 +3,15 @@ import { jsx, Flex, Box } from "theme-ui";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { MenuCamel } from "../../components";
+import { ProfileIcon } from "../../components/ProfileIcon";
 import { Link } from "../../components/NavItems";
 import { Menu, Sidebar, Container, Icon } from "semantic-ui-react";
+import { selectUser } from "../../store";
 
 export default props => {
   const auth = useSelector(state => state.auth);
   const [open, setOpen] = useState(false);
-
-  // const sidebarItems = pages.map(page => (
-  //   <Menu.Item
-  //     key={page.pageId}
-  //     link
-  //     to={`/${page.name}`}
-  //     as={Link}
-  //     activeClassName="nactive"
-  //     onClick={() => setOpen(false)}
-  //     sx={{
-  //       variant: "links.nav",
-  //     }}
-  //     p={2}
-  //   >
-
-  //   </Menu.Item>
-  // ));
+  const user = useSelector(selectUser);
 
   return (
     <Sidebar.Pushable>
@@ -37,27 +23,52 @@ export default props => {
         vertical
         visible={open}
       >
-        <Menu.Item>
+        <Menu.Item
+          style={{ fontSize: "1.9em", fontWeight: "bold" }}
+          as={Link}
+          to="/"
+          onClick={() => setOpen(false)}
+        >
           <Flex sx={{ alignItems: "center" }}>
             <MenuCamel />
-            <Menu.Item as={Link} to="/" onClick={() => setOpen(false)}>
-              Camel Case
-            </Menu.Item>
+            Camel Case
             <Box mx="auto" />
           </Flex>
         </Menu.Item>
+        {auth.token && (
+          <Menu.Item
+            style={{ fontSize: "1.25em" }}
+            as={Link}
+            to={`/u/${user.username}`}
+          >
+            <Flex sx={{ alignItems: "center" }}>
+              <ProfileIcon userId={user.userId} />
+              <span sx={{ p: "0.2em" }} />
+              {user.firstName} {user.lastName}
+              <Box mx="auto" />
+            </Flex>
+          </Menu.Item>
+        )}
         <Menu.Item as={Link} to="themes" onClick={() => setOpen(false)}>
+          <Icon name="paint brush" />
           Themes
         </Menu.Item>
-
-        {/* {auth.token && <ProfileDropdown items="default" />} */}
-
+        {auth.token && (
+          <React.Fragment>
+            <Menu.Item as={Link} to="/logout" onClick={() => setOpen(false)}>
+              <Icon name="sign out" />
+              Logout
+            </Menu.Item>
+          </React.Fragment>
+        )}
         {!auth.token && (
           <React.Fragment>
             <Menu.Item as={Link} to="login" onClick={() => setOpen(false)}>
+              <Icon name="sign in" />
               Login
             </Menu.Item>
             <Menu.Item as={Link} to="signup" onClick={() => setOpen(false)}>
+              <Icon name="signup" />
               Sign up
             </Menu.Item>
           </React.Fragment>
@@ -72,9 +83,6 @@ export default props => {
           </Menu>
         </Container>
         {props.body}
-        {/* <Flex variant="layout.centerflex">
-          <Whitebox children={props.children} />
-        </Flex> */}
       </Sidebar.Pusher>
     </Sidebar.Pushable>
   );
