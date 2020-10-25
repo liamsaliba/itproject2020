64b01d0baa8f2142bedf3a6ef4a14b96992414dc
Liam Saliba <liamsaliba@hotmail.com>
2020-10-26 06:11:53 +1100

    update page manager

--- client/src/pages/Editor/SectionSettings.jsx
+++ client/src/pages/Editor/SectionSettings.jsx
@@ -1,38 +1,10 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import Section from "./Section";
-import { Checkbox, Button, Icon } from "semantic-ui-react";
-import { selectUser, updateUser } from "../../store";
-import { useDispatch, useSelector } from "react-redux";
+import { Button, Icon } from "semantic-ui-react";
 import { Link } from "../../components";
-import { isTrue } from "../../helpers";
-
-const Toggle = ({ value, setValue, icon, label }) => {
-  const onChange = (_, { checked }) => {
-    // set to the new value of checked
-    setValue(checked.toString());
-  };
-
-  return (
-    <Checkbox
-      toggle
-      onChange={onChange}
-      checked={isTrue(value)}
-      label={
-        <label>
-          {icon ? <Icon name={icon} /> : null}
-          {label}
-        </label>
-      }
-    />
-  );
-};
 
 const SectionSettings = () => {
-  const singlePages = useSelector(state => selectUser(state).useSinglePages);
-  const dispatch = useDispatch();
-  const contact = useSelector(state => selectUser(state).allowContact);
-
   return (
     <Section name="Settings" icon="settings">
       <div
@@ -42,22 +14,6 @@ const SectionSettings = () => {
           },
         }}
       >
-        <div sx={{ textAlign: "left" }}>
-          <Toggle
-            label="Enable contact form"
-            icon="address book"
-            value={contact}
-            setValue={value => dispatch(updateUser({ allowContact: value }))}
-          />
-        </div>
-        <div sx={{ textAlign: "left" }}>
-          <Toggle
-            label="Use single pages"
-            icon="file"
-            value={singlePages}
-            setValue={value => dispatch(updateUser({ useSinglePages: value }))}
-          />
-        </div>
         <div>
           <Button icon primary labelPosition="left" as={Link} to="/settings">
             <Icon name="settings" />
