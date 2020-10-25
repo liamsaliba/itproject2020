8cafdc9a79d3a6895fac269eb36fcb55cb8b2a46
Liam Saliba <liamsaliba@hotmail.com>
2020-10-25 19:29:45 +1100

    add new toggles

--- client/src/pages/Editor/SectionSettings.jsx
+++ client/src/pages/Editor/SectionSettings.jsx
@@ -1,43 +1,69 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import Section from "./Section";
-import { ThemeSelector } from "../../components";
-import { Checkbox, Icon } from "semantic-ui-react";
-import {
-  changePortfolioTheme,
-  selectCurrentUserPortfolio,
-  selectUser,
-  updateUser,
-} from "../../store";
+import { Checkbox, Button, Icon } from "semantic-ui-react";
+import { selectUser, updateUser } from "../../store";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "../../components";
+import { isTrue } from "../../helpers";
 
-const ContactSwitch = () => {
-  const contact = useSelector(state => selectUser(state).allowContact);
-  const dispatch = useDispatch();
-
-  const onChange = (e, { checked }) => {
-    dispatch(updateUser({ allowContact: !checked }));
+const Toggle = ({ value, setValue, icon, label }) => {
+  const onChange = (_, { checked }) => {
+    // set to the new value of checked
+    setValue(checked.toString());
   };
 
-  return <Checkbox toggle onChange={onChange} checked={contact} />;
+  return (
+    <Checkbox
+      toggle
+      onChange={onChange}
+      checked={isTrue(value)}
+      label={
+        <label>
+          {icon ? <Icon name={icon} /> : null}
+          {label}
+        </label>
+      }
+    />
+  );
 };
 
 const SectionSettings = () => {
+  const singlePages = useSelector(state => selectUser(state).useSinglePages);
   const dispatch = useDispatch();
-  const portfolio = useSelector(selectCurrentUserPortfolio);
-  const setTheme = theme => dispatch(changePortfolioTheme(theme));
+  const contact = useSelector(state => selectUser(state).allowContact);
 
   return (
     <Section name="Settings" icon="settings">
-      <div>
-        <Icon name="paint brush" />
-        Theme
-        <ThemeSelector theme={portfolio.theme} setTheme={setTheme} />
-      </div>
-      <div>
-        <Icon name="address book" />
-        Contact form
-        <ContactSwitch />
+      <div
+        sx={{
+          "& div": {
+            mb: "1em",
+          },
+        }}
+      >
+        <div sx={{ textAlign: "left" }}>
+          <Toggle
+            label="Enable contact form"
+            icon="address book"
+            value={contact}
+            setValue={value => dispatch(updateUser({ allowContact: value }))}
+          />
+        </div>
+        <div sx={{ textAlign: "left" }}>
+          <Toggle
+            label="Use single pages"
+            icon="file"
+            value={singlePages}
+            setValue={value => dispatch(updateUser({ useSinglePages: value }))}
+          />
+        </div>
+        <div>
+          <Button icon primary labelPosition="left" as={Link} to="/settings">
+            <Icon name="settings" />
+            User Settings
+          </Button>
+        </div>
       </div>
     </Section>
   );
