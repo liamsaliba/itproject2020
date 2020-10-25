23128e1526f554634d97f4fb6d88827deb99f412
Liam Saliba <liamsaliba@hotmail.com>
2020-10-26 06:09:55 +1100

    add use path helper

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,1 +1,19 @@
+import { useSelector } from "react-redux";
+import { selectPortfolioIsEditing, selectPortfolioByUsername } from "./store";
+
 export const isTrue = value => value === "true" || value === true;
+
+export const usePath = userId => {
+  const editing = useSelector(state => selectPortfolioIsEditing(state, userId));
+
+  const singlePage = useSelector(
+    state => selectPortfolioByUsername(state, userId).singlePage
+  );
+  const path = editing ? "/editor" : `/u/${userId}`;
+  const sep = singlePage ? "#" : "";
+
+  return pageName =>
+    pageName === "" || pageName === undefined
+      ? path
+      : `${path}/${sep}${pageName}`;
+};
