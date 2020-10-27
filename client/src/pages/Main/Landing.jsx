cfefdec2ad38aa598f24ef9f8e088374c73bc310
Chan Jie Ho <hochanjie@hotmail.com>
2020-10-27 14:42:49 +1100

    Replaced landing page placeholders and added name to portfolio footer

--- client/src/pages/Main/Landing.jsx
+++ client/src/pages/Main/Landing.jsx
@@ -13,7 +13,15 @@ import {
   Divider,
 } from "semantic-ui-react";
 import camel from "../../svg/camel.svg";
+import autosave from "../../svg/Landing/Autosave.png";
+import customise from "../../svg/Landing/Customise.png";
+import media from "../../svg/Landing/Media.png";
+import portfolio from "../../svg/Landing/Portfolio.png";
+import themes from "../../svg/Landing/Themes.png";
+
 import { Link } from "react-router-dom";
+
+// Illustration by Freepik Stories "https://stories.freepik.com/web"
 
 const HomepageHeading = () => (
   <Segment textAlign="center" style={{ padding: "1em 0em" }} vertical>
@@ -65,49 +73,78 @@ const HomepageHeading = () => (
 const HomepageBody = () => (
   <React.Fragment>
     <Segment style={{ padding: "1em 0em" }} vertical>
-      <Grid
-        container
-        style={{ marginTop: "4em", marginBottom: "6em" }}
-        centered
-        verticalAlign="middle"
-        stackable
-      >
+      <Grid container centered verticalAlign="middle" stackable>
         <Grid.Row>
           <Grid.Column width={8}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Create and share ePortfolios on the fly
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Creating your own ePortfolio on the whim and sharing with
+              potential employers or friends have never been easier with a
+              simple, easy-to-use UI.
+            </p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={6}>
+            <Image
+              rounded
+              size="large"
+              src={portfolio}
+              href="https://stories.freepik.com/web"
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: "1em 0em" }} vertical>
+      <Grid container centered verticalAlign="middle" stackable>
+        <Grid.Row>
+          <Grid.Column floated="left" width={6}>
+            <Image
+              rounded
+              size="large"
+              src={customise}
+              href="https://stories.freepik.com/web"
+            />
+          </Grid.Column>
+          <Grid.Column textAlign="right" width={8}>
             <Header as="h3" style={{ fontSize: "2em" }}>
               Hit the ground running
             </Header>
             <p style={{ fontSize: "1.33em" }}>
               Pick a ready-made template without having to worry about complex
               or complicated features while still being able to customise it
-              enough to call it your’s.
+              enough to call it yours.
             </p>
-          </Grid.Column>
-          <Grid.Column floated="right" width={6}>
-            <Image rounded size="large" src={camel} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Segment>
+
     <Segment style={{ padding: "1em 0em" }} vertical>
       <Grid
         container
-        style={{ marginTop: "4em", marginBottom: "6em" }}
+        style={{ marginTop: "0em", marginBottom: "1em" }}
         centered
         verticalAlign="middle"
         stackable
       >
         <Grid.Row>
-          <Grid.Column floated="left" width={6}>
-            <Image rounded size="large" src={camel} />
-          </Grid.Column>
-          <Grid.Column textAlign="right" width={8}>
+          <Image
+            rounded
+            size="large"
+            src={themes}
+            href="https://stories.freepik.com/web"
+          />
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column textAlign="center" width={8}>
             <Header as="h3" style={{ fontSize: "2em" }}>
               An eye for aesthetics
             </Header>
             <p style={{ fontSize: "1.33em" }}>
               Add colour and life to your portfolio by choosing one of our
-              favourite colour palettes.
+              favourite themes and colour palettes.
             </p>
             <Button animated secondary size="huge" as={Link} to="themes">
               <Button.Content visible>Check out our themes!</Button.Content>
@@ -115,6 +152,58 @@ const HomepageBody = () => (
                 <Icon name="arrow right" />
               </Button.Content>
             </Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: "1em 0em" }} vertical>
+      <Grid container centered verticalAlign="middle" stackable>
+        <Grid.Row>
+          <Grid.Column floated="left" width={6}>
+            <Image
+              rounded
+              size="large"
+              src={media}
+              href="https://stories.freepik.com/web"
+            />
+          </Grid.Column>
+          <Grid.Column textAlign="right" width={8}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Everything you need, all at your fingertips
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Pick and choose which of your work to show off by easily managing
+              all your media uploads at any time.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: "1em 0em" }} vertical>
+      <Grid container centered verticalAlign="middle" stackable>
+        <Grid.Row>
+          <Grid.Column width={6}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Save as you go
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Now you don't have to worry about whether you saved your work.
+              Leave it to us!
+            </p>
+            <Button animated primary size="huge" as={Link} to="signup">
+              <Button.Content visible>Get Started!</Button.Content>
+              <Button.Content hidden>
+                <Icon name="user plus" />
+              </Button.Content>
+            </Button>
+          </Grid.Column>
+          <Grid.Column floated="right" width={6}>
+            <Image
+              rounded
+              size="large"
+              src={autosave}
+              href="https://stories.freepik.com/web"
+            />
           </Grid.Column>
         </Grid.Row>
       </Grid>
--- client/src/pages/User/Footer.jsx
+++ client/src/pages/User/Footer.jsx
@@ -1,13 +1,18 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { MenuItem, Navbar } from "../../components";
+import { selectFullName } from "../../store";
+import { useSelector } from "react-redux";
 
 export default props => {
+  const fullName = useSelector(state => selectFullName(state, props.userId));
   return (
     <Navbar stackable style={{ border: "none" }} attached="bottom">
       <Navbar.Left>
         <MenuItem to="/">camel_case</MenuItem>
-        <div sx={{ p: 2 }}>/ {props.userId}</div>
+        <div sx={{ p: 2 }}>
+          {fullName} / {props.userId}
+        </div>
       </Navbar.Left>
       <Navbar.Right>
         <MenuItem href="https://github.com/exradr/itproject2020">
