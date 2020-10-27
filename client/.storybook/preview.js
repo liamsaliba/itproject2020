c9bd0c65eb12de2d5792d7497663822a4df0b758
Lawrence Leong <lsleong@student.unimelb.edu.au>
2020-10-27 12:33:58 +1100

    Added Redux to storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,15 +2,31 @@
 import { addDecorator, addParameters } from '@storybook/react';
 
 import themes from "../src/themes";
-
 import { withThemeProvider } from 'storybook-addon-theme-ui'
 import 'semantic-ui-css/semantic.min.css'
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
 import 'react-datepicker/dist/react-datepicker.css';
 
+// ===== For React-Redux ===== 
+import { Provider } from "react-redux" //'./Provider.js'
+import configureStore from '../src/store/configureStore.js';
+import { jsx } from 'theme-ui';
+
+const store = configureStore();
+
+const WithProvider = ({ story }) => (
+  <Provider store={store}>
+      { story() }
+  </Provider>
+);
 
 addDecorator(withThemeProvider)
+addDecorator(story => (
+    <WithProvider story={story}/>
+  )
+)
+
+
 
 
 export const parameters = {
