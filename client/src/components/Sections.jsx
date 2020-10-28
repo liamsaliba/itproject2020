e2a2117122c8a994852117ee4ce04de6dd1164df
Lawrence Leong <45556912+Rexrover2@users.noreply.github.com>
2020-10-28 15:31:41 +1100

    Update Sections.jsx
    Reverted foreground text to white
    changing filter opacity to 70%

--- client/src/components/Sections.jsx
+++ client/src/components/Sections.jsx
@@ -180,7 +180,7 @@ const MediaCollection = ({ media, darken = false }) => {
   const mediaStyle = {
     boxShadow: "0 0 3px rgba(0, 0, 0, 0.125)",
     // maxWidth: "390px",
-    filter: darken ? "brightness(0.95)" : undefined,
+    filter: darken ? "brightness(0.70)" : undefined,
     width: "100%",
     height: "100%",
     objectFit: "cover",
@@ -271,9 +271,11 @@ export const StyledArtifact = ({
             position: "relative",
             width: "100%",
             zIndex: "1",
-            color: media.length !== 0 ? "black" : undefined,
+            color: media.length !== 0 ? "
+            
+            " : undefined,
             "& h1, & h2, & h3, & p": {
-              color: media.length !== 0 ? "black" : undefined,
+              color: media.length !== 0 ? "white" : undefined,
             },
           }}
         >
