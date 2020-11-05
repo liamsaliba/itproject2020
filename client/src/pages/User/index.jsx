69b33b1c107f214c9f3abca921ec9c8d283d4b08
Liam Saliba <liamsaliba@hotmail.com>
2020-11-05 22:10:43 +1100

    fix flash of not found

--- client/src/pages/User/index.jsx
+++ client/src/pages/User/index.jsx
@@ -2,7 +2,7 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { NotExist } from "./NotExist";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {
   fetchEntirePortfolio,
@@ -24,12 +24,21 @@ const User = props => {
   // eslint-disable-next-line
   const editing = useSelector(selectPortfolioIsEditing);
   const loading = useSelector(state => selectPortfoliosSlice(state).loading);
+  const [status, setStatus] = useState("waitload");
   // const editing = props.editing || false;
 
   useEffect(() => {
     dispatch(fetchEntirePortfolio(userId));
     dispatch(changePortfolio(userId));
   }, [userId]);
+
+  useEffect(() => {
+    if (loading) {
+      setStatus("loading");
+    } else if (status === "loading") {
+      setStatus("doneload");
+    }
+  });
 
   const portfolio = useSelector(state =>
     selectPortfolioByUsername(state, userId)
@@ -38,7 +47,7 @@ const User = props => {
 
   return portfolio ? (
     <UserPage userId={userId} selectedPage={selectedPage} />
-  ) : loading ? (
+  ) : status !== "doneload" ? (
     <Dimmer active={loading} inverted>
       <Loader inverted>
         <Camel src={avatar} />
