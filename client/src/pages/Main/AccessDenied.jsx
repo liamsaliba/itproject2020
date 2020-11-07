e95e65994061d358a36277b9ce2e53dcccd51918
Chan Jie Ho <hochanjie@hotmail.com>
2020-11-07 19:55:31 +1100

    Send users to access denied page if accessing editor on mobile

---
+++ client/src/pages/Main/AccessDenied.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Styled, Box } from "theme-ui";
+import { Header, Icon } from "semantic-ui-react";
+import { MenuButton } from "../../components";
+
+// TODO: Styles
+
+export default () => {
+  return (
+    <Box>
+      <Header as="h2">
+        <Icon name="remove" />
+        <Header.Content>Access Denied</Header.Content>
+      </Header>
+      <Styled.p>Mobile editting is not allowed</Styled.p>
+      <MenuButton to={`/`}>
+        <Icon name="home" />
+        Home
+      </MenuButton>
+    </Box>
+  );
+};
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Editor } from "./Editor";
 export { default as Main } from "./Main";
 export { default as User } from "./User";
+export { default as NotFound } from "./Main/NotFound";
