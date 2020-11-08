5a3b7859c504ad55d55798cdebd78a495aa13ec2
Liam Saliba <liamsaliba@hotmail.com>
2020-11-08 12:35:10 +1100

    Finish password reset page

--- client/src/pages/Main/ResetPassword.jsx
+++ client/src/pages/Main/ResetPassword.jsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import { jsx, Flex } from "theme-ui";
-import { Form, Grid, Icon, Header, Message } from "semantic-ui-react";
+import { jsx, Box } from "theme-ui";
+import { Form, Grid, Icon, Header, Message, Loader } from "semantic-ui-react";
 import { toast } from "react-toastify";
 import ReactCodeInput from "react-verification-code-input";
 import React from "react";
@@ -11,47 +11,105 @@ import React from "react";
 import { Title } from "../../components";
 
 import { useState } from "react";
+import { endpoints, selectUsername } from "../../store";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import { useFormState } from "../../components/Modals";
 
 export default () => {
-  const [valid, setValid] = useState(false);
-  const [visible, setVisible] = useState(true);
-  // const authError = useSelector(state => selectAuthSlice(state).error);
+  const [status, setStatus] = useState("wait username");
+  const { state, handleChange } = useFormState({
+    password: "",
+    confirmPassword: "",
+    code: "",
+  });
 
-  // TODO: change hardcoded value to the code sent to email
-  const checkVal = val => {
-    if (val === "123456") {
-      setValid(true);
-    } else {
-      toast.error("Invalid code");
+  const loggedInUsername = useSelector(selectUsername);
+  const [username, setUsername] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (status === "wait username") {
+      if (loggedInUsername === undefined) {
+        setStatus("ask username");
+      } else {
+        setUsername(loggedInUsername);
+        setStatus("got username");
+      }
     }
-  };
+  }, [status, loggedInUsername]);
 
-  // useEffect(() => {
-  //   // TODO: fix error handling on store
-  //   if (authError) {
-  //     toast.error(
-  //       <Toast
-  //         title="Couldn't update user account."
-  //         message={authError.data}
-  //         technical={authError.message}
-  //       />
-  //     );
-  //   }
-  // }, [authError]);
+  useEffect(() => {
+    if (status === "got username") {
+      console.log("sending email to ", username);
+      axios
+        .post(
+          endpoints.resetPasswordStep(1),
+          { username },
+          { baseURL: endpoints.baseURL }
+        )
+        .then(res => {
+          setStatus("await code");
+        })
+        .catch(err => {
+          console.log("search err", err.message, err.response);
+          setStatus("fail");
+        });
+    }
+  }, [username, status]);
 
-  const handleSubmit = e => {
+  const handleUsernameSubmit = e => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const password = formData.get("newPassword");
-    const confirmPassword = formData.get("confirmNewPassword");
-    if (password === "" || confirmPassword === "") {
+    const username = formData.get("username");
+    if (username === "") {
       toast.error("Required fields are empty.");
+      return;
+    }
+    setUsername(username);
+    setStatus("got username");
+    // dispatch(updateUser({ firstName, lastName }));
+  };
+
+  const handlePasswordSubmit = e => {
+    e.preventDefault();
+    const { password, code, confirmPassword } = state;
+    if (code === "") {
+      toast.error("Verification code not entered.");
+    }
+
+    if (password === "" || confirmPassword === "") {
+      toast.error("Password not entered.");
       return;
     }
     if (confirmPassword !== password) {
       toast.error("Password does not match.");
       return;
     }
+    console.log("submitting...");
+    axios
+      .post(
+        endpoints.resetPasswordStep(2),
+        { username, password, code },
+        { baseURL: endpoints.baseURL }
+      )
+      .then(res => {
+        setStatus("success");
+        if (loggedInUsername === null) {
+          toast.success("Password reset! Try logging in.");
+          setTimeout(() => history.push("/login"), 200);
+        } else {
+          toast.success("Password reset!");
+          setTimeout(() => history.push("/settings"), 200);
+        }
+      })
+      .catch(err => {
+        console.log("err", err.message, err.response);
+        // todo reset code.
+        setStatus("fail");
+      });
     // dispatch(updateUser({ firstName, lastName }));
   };
 
@@ -61,84 +119,123 @@ export default () => {
         <Grid.Column verticalAlign="middle" style={{ minWidth: 400 }}>
           <Title>Reset Password</Title>
           <Header as="h2" textAlign="center">
-            <Icon name="user secret" /> Reset/Change Password
+            <Icon name="user secret" /> Reset Password
           </Header>
+        </Grid.Column>
+      </Grid.Row>
+      {status === "await code" ? (
+        <React.Fragment>
+          <Grid.Row centered>
+            <Grid.Column style={{ maxWidth: 400 }}>
+              <Message textAlign="center">
+                Enter the 6-digit verification code sent to your email and
+                change your password.
+              </Message>
+              <Box
+                sx={{
+                  m: "2em auto",
+                  "& > div": {
+                    m: "0 auto",
+                  },
+                }}
+              >
+                <ReactCodeInput
+                  value={state.code}
+                  onChange={value =>
+                    handleChange(null, { name: "code", value })
+                  }
+                  inputStyle={{ margin: "0 auto" }}
+                />
+              </Box>
+              <Form size="large" onSubmit={handlePasswordSubmit}>
+                <Form.Input
+                  name="password"
+                  value={state.password}
+                  onChange={handleChange}
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="New Password"
+                  type="password"
+                />
+                <Form.Input
+                  name="confirmPassword"
+                  value={state.confirmPassword}
+                  onChange={handleChange}
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Confirm Password"
+                  type="password"
+                />
+                <br />
+                <Form.Group widths="equal">
+                  <Form.Button
+                    fluid
+                    basic
+                    negative
+                    icon="arrow left"
+                    content="Cancel"
+                    type="button"
+                    onClick={() => history.goBack()}
+                  />
+                  <Form.Button
+                    fluid
+                    positive
+                    icon="check"
+                    content="Save"
+                    type="submit"
+                    // onClick={() => setEditing(false)}
+                  />
+                </Form.Group>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
           <br />
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row centered>
-        <Grid.Column style={{ maxWidth: 400 }}>
-          {!valid ? (
-            <React.Fragment>
-              <Header as="h2" textAlign="center">
-                Enter the 6-digit verification code sent to your email
-              </Header>
-              <Flex variant="layout.centerflex">
-                <ReactCodeInput onComplete={val => checkVal(val)} />
-              </Flex>
-            </React.Fragment>
-          ) : (
-            <Form size="large" onSubmit={handleSubmit}>
-              <Form.Input
-                name="password"
-                fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="New Password"
-                type="password"
-              />
-              <Form.Input
-                name="confirmPassword"
-                fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="Confirm Password"
-                type="password"
-              />
-              <Form.Group widths="equal">
-                <Form.Button
-                  fluid
-                  basic
-                  negative
-                  icon="remove"
-                  content="Cancel"
-                  // onClick={() => setEditing(false)}
-                />
-                <Form.Button
-                  fluid
-                  positive
-                  icon="check"
-                  content="Save"
-                  type="submit"
-                  // onClick={() => setEditing(false)}
-                />
-              </Form.Group>
-            </Form>
-          )}
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        {!visible ? null : (
-          <Flex variant="layout.centerflex">
-            <Message
-              onDismiss={() => setVisible(false)}
-              // header='A link to change your password has been sent to your email!'
-              attached="bottom"
-              content="A link to change your password has been sent to your email!"
-              info
+        </React.Fragment>
+      ) : status === "got username" ? (
+        <Loader inline size="large" active={true}>
+          Sending email...
+        </Loader>
+      ) : status === "ask username" ? (
+        <Box sx={{ minWidth: "350px" }}>
+          <Form size="large" onSubmit={handleUsernameSubmit}>
+            <Message>
+              An email will be sent to you with a verification code. <br /> You
+              can then enter that code to change your password.
+            </Message>
+            <Header as="h3" textAlign="center">
+              Enter your username
+            </Header>
+            <Form.Input
+              name="username"
               fluid
+              icon="at"
+              iconPosition="left"
+              placeholder="username"
             />
-          </Flex>
-        )}
-      </Grid.Row>
+            <Form.Group widths="equal">
+              <Form.Button
+                fluid
+                basic
+                type="button"
+                negative
+                icon="arrow left"
+                content="Cancel"
+                onClick={() => history.goBack()}
+              />
+              <Form.Button
+                fluid
+                positive
+                icon="send"
+                content="Send email"
+                type="submit"
+                // onClick={() => setEditing(false)}
+              />
+            </Form.Group>
+          </Form>
+        </Box>
+      ) : null}
     </Grid>
   );
 };
